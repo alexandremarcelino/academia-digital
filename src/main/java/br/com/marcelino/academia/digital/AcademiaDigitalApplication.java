8e789a450256068a201cfd4f49fce82b4f33f826
Alexandre Marcelino <alexandre.hoffmann.marcelino@gmail.com>
2023-10-18 19:41:28 -0300

    Teste

--- src/main/java/br/com/marcelino/academia/digital/AcademiaDigitalApplication.java
+++ src/main/java/br/com/marcelino/academia/digital/AcademiaDigitalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AcademiaDigitalApplication {
 
 	public static void main(String[] args) {
+		//Teste
 		SpringApplication.run(AcademiaDigitalApplication.class, args);
 	}
 
